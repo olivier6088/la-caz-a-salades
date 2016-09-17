5267b7bdd52f4c9b544914b42ba6584c5bcc9e8c
olivier6088 <olivier.savriama@gmail.com>
2016-09-17 23:43:01 +0400

    Start using angularJS

---
+++ site/js/main.js
@@ -0,0 +1,41 @@
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('myApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider, $httpProvider) {
+  
+  // This line is for browser-sync only (to be removed)
+  //$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+  $routeProvider
+    // Accueil
+    .when("/", {templateUrl: "snippets/accueil.html", controller: "PageCtrl"})
+    // Pages
+    .when("/apropos", {templateUrl: "snippets/apropos.html", controller: "PageCtrl"})
+    .when("/carte", {templateUrl: "snippets/carte.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "snippets/contact.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "snippets/404.html", controller: "PageCtrl"});
+}]);
+
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').bcSwipe({ threshold: 50 });
+
+});
