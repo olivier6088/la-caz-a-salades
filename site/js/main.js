399953f227ef3c75073c9f70c46d16ead8e67811
olivier6088 <olivier.savriama@gmail.com>
2016-09-18 10:29:53 +0400

    Routes are OK

--- site/js/main.js
+++ site/js/main.js
@@ -1,32 +1,41 @@
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('myApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-]);
+var app = angular.module('myApp', ['ngRoute']);
 
 /**
  * Configure the Routes
  */
-app.config(['$routeProvider', function ($routeProvider, $httpProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
   
   // This line is for browser-sync only (to be removed)
   //$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
   $routeProvider
     // Accueil
-    .when("/", {templateUrl: "snippets/accueil.html", controller: "PageCtrl"})
+    .when('/', {
+      templateUrl: 'snippets/accueil.html',
+      controller: 'PageCtrl'
+    })
     // Pages
-    .when("/apropos", {templateUrl: "snippets/apropos.html", controller: "PageCtrl"})
-    .when("/carte", {templateUrl: "snippets/carte.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "snippets/contact.html", controller: "PageCtrl"})
+    .when('/apropos', {
+      templateUrl: 'snippets/apropos.html',
+      controller: 'PageCtrl'
+    })
+    .when('/carte', {
+      templateUrl: 'snippets/carte.html',
+      controller: 'PageCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'snippets/contact.html',
+      controller: 'PageCtrl'
+    })
     // else 404
-    .otherwise("/404", {templateUrl: "snippets/404.html", controller: "PageCtrl"});
+    .otherwise('/404', {
+      templateUrl: 'snippets/404.html',
+      controller: 'PageCtrl'
+    });
 }]);
-
 
 /**
  * Controls all other Pages
